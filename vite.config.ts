ec43d58efaae36031fe72d5df989b1acbcaca641
Lorenzo <lorenzotrevizoldalbo@gmail.com>
2025-12-18 14:04:40 -0300

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/YOUR_REPO_NAME/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
